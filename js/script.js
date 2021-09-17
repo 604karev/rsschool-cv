502c293836f58f5f9c3eff59013888a42247629c
604karev <604karev@gmail.com>
2021-09-17 10:33:05 +0300

    feat: add sidebar navigation menu

--- js/script.js
+++ js/script.js
@@ -3,20 +3,16 @@ const burger = document.querySelector('.burger');
 const sideBarWrapper = document.querySelector('.sidebar-wrapper');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
+const nav = document.querySelector('.s-nav');
 
 const toggleSidebar = (e) => {
-    sideBar.classList.toggle('active');
     body.classList.toggle('active');
     sideBarWrapper.classList.remove('visible');
-    if(document.body.clientWidth >= window.innerWidth){
-
-    }
-
 };
 
 const toggleInformation = (e) => {
     e.stopPropagation();
-    if (window.innerWidth > 1140 || sideBar.classList.contains('active')) {
+    if (window.innerWidth > 1140 || body.classList.contains('active')) {
         sideBarWrapper.classList.add('visible');
     } else {
         sideBarWrapper.classList.remove('visible')
@@ -25,7 +21,7 @@ const toggleInformation = (e) => {
 
 
 const removeVisible = () => {
-    if (window.innerWidth <= 1140 && !sideBar.classList.contains('active')) {
+    if (window.innerWidth <= 1140 && !body.classList.contains('active')) {
         sideBarWrapper.classList.remove('visible');
     }
 };
@@ -35,7 +31,14 @@ const checkVisible = () => {
         sideBarWrapper.classList.add('visible')
     }
 };
+const removeActive = (e) => {
+    if (e.target.classList.contains('aside-list__link') && window.innerWidth <= 1140) {
+        sideBarWrapper.classList.remove('visible');
+        body.classList.remove('active');
+    }
+};
 
+nav.addEventListener('click', removeActive);
 overlay.addEventListener('click', toggleSidebar);
 burger.addEventListener('click', toggleSidebar);
 sideBar.addEventListener('transitionend', toggleInformation);
