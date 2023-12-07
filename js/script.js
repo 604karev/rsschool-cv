71d49144a20ce2e6bbdff1be5c298dbde6ccfdc2
Kirill Hrudzinski <604karev@gmail.com>
2023-12-07 13:24:13 +0100

    fix print page bug

--- js/script.js
+++ js/script.js
@@ -1,68 +1,72 @@
-
-
-const sideBar = document.querySelector('.sidebar');
-const burger = document.querySelector('.burger');
-const sideBarWrapper = document.querySelector('.sidebar-wrapper');
-const html = document.querySelector('html');
-const body = document.querySelector('body');
-const nav = document.querySelector('.s-nav');
-const pictureWrapper = document.querySelector('.picture-wrapper');
-
+const sideBar = document.querySelector(".sidebar");
+const burger = document.querySelector(".burger");
+const sideBarWrapper = document.querySelector(".sidebar-wrapper");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+const nav = document.querySelector(".s-nav");
+const pictureWrapper = document.querySelector(".picture-wrapper");
+const printMenu = document.querySelector(".print");
 
 const toggleSidebar = (e) => {
-    html.classList.toggle('active');
-    sideBarWrapper.classList.remove('visible');
-    /*console.log(document.body.clientWidth, window.innerWidth)*/
+  html.classList.toggle("active");
+  sideBarWrapper.classList.remove("visible");
+  /*console.log(document.body.clientWidth, window.innerWidth)*/
 };
 
 const toggleInformation = (e) => {
-    e.stopPropagation();
-    if (window.innerWidth > 1140 || html.classList.contains('active')) {
-        sideBarWrapper.classList.add('visible');
-    } else {
-        sideBarWrapper.classList.remove('visible')
-    }
+  e.stopPropagation();
+  if (window.innerWidth > 1140 || html.classList.contains("active")) {
+    sideBarWrapper.classList.add("visible");
+  } else {
+    sideBarWrapper.classList.remove("visible");
+  }
 };
 
-
 const removeVisible = () => {
-    if (window.innerWidth <= 1140 && !html.classList.contains('active')) {
-        sideBarWrapper.classList.remove('visible');
-    }
+  if (window.innerWidth <= 1140 && !html.classList.contains("active")) {
+    sideBarWrapper.classList.remove("visible");
+  }
 };
 
 const checkVisible = () => {
-    if (window.innerWidth > 1140) {
-        sideBarWrapper.classList.add('visible')
-    }
+  if (window.innerWidth > 1140) {
+    sideBarWrapper.classList.add("visible");
+  }
 };
 const removeActive = (e) => {
-    if (e.target.classList.contains('aside-list__link') && window.innerWidth <= 1140 || e.target === body && window.innerWidth <= 1140) {
-        sideBarWrapper.classList.remove('visible');
-        html.classList.remove('active');
-    }
+  if (
+    (e.target.classList.contains("aside-list__link") &&
+      window.innerWidth <= 1140) ||
+    (e.target === body && window.innerWidth <= 1140)
+  ) {
+    sideBarWrapper.classList.remove("visible");
+    html.classList.remove("active");
+  }
 };
-const createImage = (src) => new Promise((res, rej) => {
+const createImage = (src) =>
+  new Promise((res, rej) => {
     const img = new Image();
     img.onload = () => res(img);
     img.onerror = rej;
     img.src = src;
-});
+  });
 
 async function render() {
-    const myImage = await createImage('./assets/img/kirill-hrudzinski.png');
-    myImage.classList.add('cv-picture');
-    myImage.alt = 'kirill-hrudzinski';
-    pictureWrapper.appendChild(myImage);
+  const myImage = await createImage("./assets/img/kirill-hrudzinski.png");
+  myImage.classList.add("cv-picture");
+  myImage.alt = "kirill-hrudzinski";
+  pictureWrapper.appendChild(myImage);
 }
+const printDocument = () => {
+  html.classList.remove("active");
+  sideBarWrapper.classList.remove("visible");
+  window.print();
+};
 
-render();
-
-nav.addEventListener('click', removeActive);
-body.addEventListener('click', removeActive);
-burger.addEventListener('click', toggleSidebar);
-sideBar.addEventListener('transitionend', toggleInformation);
-window.addEventListener('resize', removeVisible);
-window.addEventListener('load', checkVisible);
-
-
+nav.addEventListener("click", removeActive);
+body.addEventListener("click", removeActive);
+burger.addEventListener("click", toggleSidebar);
+sideBar.addEventListener("transitionend", toggleInformation);
+window.addEventListener("resize", removeVisible);
+window.addEventListener("load", checkVisible, render());
+printMenu.addEventListener("click", printDocument);
